b4fdeb32e60af2572b6481e1e37c0a056c27ae6d
aalsarori <55166509+aalsarori@users.noreply.github.com>
2022-07-20 17:57:28 -0600

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+/*
+namespace ajax.Pages;*/
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult OnPostGetAjax(string name)
+    {
+        return new JsonResult("Hello " + name);
+    }
+
+    public IActionResult OnPostDoubleMoney(string description, int value)
+    {
+        if (description == "please")
+        {
+            return new JsonResult("Money doubled to: " + (value * 2));
+        }
+        else
+        {
+            return new JsonResult("Money multipled to: " + (value * 10));
+        }
+
+    }
+    public void OnGet()
+    {
+
+    }
+}
