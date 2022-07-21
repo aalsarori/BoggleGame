967535f02941809c75014971142915a9d0e80ced
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-07-20 18:11:49 -0600

    | First Commit |

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,6 +11,22 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+    // Receive a word
+
+    // Send back the arrays
+
+    // Send back the point totals
+
+    // Check to see if the word exists
+
+    // Add points equal to the length of the verified word
+
+    // Get the random words
+
+    /// <summary>
+    /// We also need to figure out how to do SignalR and time the match and end it at a certain time
+    /// </summary>
+
     public void OnGet()
     {
 
