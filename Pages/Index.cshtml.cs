d84d1da389061ef267d913089701bf873dfea60b
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-07-20 18:58:08 -0600

    | Random words sending |

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 
 public class IndexModel : PageModel
 {
@@ -12,16 +14,138 @@ public class IndexModel : PageModel
     }
 
     // Receive a word
+    public string ReceiveWord(string word)
+    {
+        return word;
+    }
 
     // Send back the arrays
+    public List<List<char>> SendArrays(List<List<char>> returnList)
+    {
+        return returnList;
+    }
 
     // Send back the point totals
+    public string SendPoints
+    {
+        get
+        {
+            return "";
+        }
+    }
 
     // Check to see if the word exists
+    public string ValidateExistence(string word)
+    {
+        // Make query
+
+        // Run query
+
+        // See if it exists
+
+        // Get points based on length
+
+        // Add points
+
+        return "";
+    }
 
     // Add points equal to the length of the verified word
+    public string AddPoints(int length)
+    {
+        // Query for the points
 
-    // Get the random words
+        // Add the points
+
+        // Update the points
+
+        // Or just update the points to be points + length if that is possible
+
+        return "";
+    }
+
+    // Return a random consonant
+    public char RandomConsonant(string word)
+    {
+        // Make the list of consonants
+        char[] consonants = "BCDFGHJKLMNPQRSTVWXYZ".ToCharArray();
+
+        // Make the random number
+        Random rnd = new Random();
+        int randomnumber = rnd.Next(0, (consonants.Length)); // creates a number between 1 and 12
+
+        // Choose the random consonant
+        char randomchar = consonants[randomnumber];
+
+        // Use recursion until the word doesn't already contain that character
+        if (word.Contains(randomchar))
+        {
+            randomchar = RandomConsonant(word);
+        }
+
+        // Return the value
+        return randomchar;
+    }
+
+    // Return a random vowel
+    public char RandomVowel
+    {
+        get
+        {
+            // Make the list of vowels
+            char[] vowels = "AEIOU".ToCharArray();
+
+            // Make the random number
+            Random rnd = new Random();
+            int randomnumber = rnd.Next(0, (vowels.Length)); // creates a number between 1 and 12
+
+            // Choose the random consonant
+            char randomchar = vowels[randomnumber];
+
+            // Return the value
+            return randomchar;
+        }
+    }
+
+    // Make the words to send to Tans function for him to scramble around
+    public List<string> CreateWords
+    {
+        get
+        {
+            // Create the variables
+            string first = "";
+            string second = "";
+            string third = "";
+            string fourth = "";
+
+            List<string> words = new List<string>();
+            
+            // Add the vowels
+            first += RandomVowel;
+            second += RandomVowel;
+            third += RandomVowel;
+            fourth += RandomVowel;
+
+            // Add the consonants
+            for(int i = 0; i < 3; i++)
+            {
+                // Add a random consonant
+                first += RandomConsonant(first);
+                second += RandomConsonant(second);
+                third += RandomConsonant(third);
+                fourth += RandomConsonant(fourth);
+            }
+
+            // Add the four words to an array
+            words.Add(first);
+            words.Add(second);
+            words.Add(third);
+            words.Add(fourth);
+
+            // Return the array
+            return words;
+        }
+    }
 
     /// <summary>
     /// We also need to figure out how to do SignalR and time the match and end it at a certain time
