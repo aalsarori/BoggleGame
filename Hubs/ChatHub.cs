1269699cbfafa3973db2a11d87022488662f2b37
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-08-01 18:14:12 -0600

    | |

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -11,23 +11,57 @@ namespace AbdulsGame.Hubs
         }
 
         // Get the game to start
-        public async Task WaitToStart(string user, string gamecode)
+        public async Task WaitToStart(string user)
         {
-            // Insert player name into player table, set score = 0.
-
             // Insert player name into the session table
+            string insert = string.Format("INSERT INTO game_session (Game_Code,Player) VALUES (1,'{0}');", user);
+
+            // Run the query
+
+
+
+
 
             // Check if count where gamecode = gamecode of game session >= 2.
+            string check = string.Format("SELECT COUNT(*) FROM game_session WHERE Game_Code = 1;");
+            int check_result = 0;
 
-            // If it is 1, return await Clients.All.SendAsync("Wait", user, message);
-                // Make it so that it just says Waiting for another player... 1/2
+            // Get result of query
 
-            // If it is 2, return await Clients.All.SendAsync("Start", user, message);  
+
+
+
+
+            if(check_result == 1 || check_result == 0)
+            {
+                // Send this to a function that will display a waiting screen
+                await Clients.All.SendAsync("WaitingMessage");
+            }
+            else
+            {
+                // Build query
+                string getplayers = "SELECT Player FROM game_session WHERE Game_Code = 1";
+
+                // Run query
+
+                // Make the loop variables
+                string player_one = "";
+                string player_two = "";
+
+                // Make a for loop that sets the first to player_one and the second to player two
+
+                // Insert player name into player table, set score = 0.
+                string setplayers = string.Format("INSERT INTO games(usernameOne,usernameTwo) VALUES ('{0}','{1}');", player_one, player_two);
+
+                // Run the query
+
                 // Get the game board and pass it in
 
-                // Pass in the timer some how
-
                 // Pass in initial names ands scores by drawing from database and passing them in
+
+            }
+
+
         }
 
         // Get the points to update
@@ -46,6 +80,9 @@ namespace AbdulsGame.Hubs
             // Send them back
             await Clients.All.SendAsync("SendScoresUser1", user1, score1);
             await Clients.All.SendAsync("SendScoresUser2", user2, score2);
+            // or
+            await Clients.All.SendAsync("SendScores", user1, score1, user2, score2);
+
         }
 
         // Get the results passed in
