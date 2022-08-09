0ce24608df4d55a2fea34eeefdb3a2de9a187654
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-08-08 18:38:28 -0600

    | |

--- wwwroot/js/chatterbox.js
+++ wwwroot/js/chatterbox.js
@@ -3,8 +3,8 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 // Wait To Start
 // Customize code to send to WaitToStart, pass in the variables
-document.getElementById("startButton").addEventListener("click", function (event) {
-    var user = "hi";
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
     connection.invoke("WaitToStart", user).catch(function (err) {
         return console.error(err.toString());
     });
@@ -29,7 +29,7 @@ connection.on("WaitingMessage", function () {
 
 // Receive Word
 // Customize code to send to WaitToStart, pass in the variables
-document.getElementById("startButton").addEventListener("click", function (event) {
+document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("ReceiveWord", user, message).catch(function (err) {
@@ -49,7 +49,7 @@ connection.on("SendScores", function (user1, score1, user2, score2) {
 
 // Results Screen
 // Customize code to send to WaitToStart, pass in the variables
-document.getElementById("startButton").addEventListener("click", function (event) {
+document.getElementById("sendButton").addEventListener("click", function (event) {
     connection.invoke("SendResultScreen").catch(function (err) {
         return console.error(err.toString());
     });
