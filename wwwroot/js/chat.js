969a79f3b344c64d2a30e28a0ac8a859d11f6093
Keanu Hansen <keanu.jon.hansen@gmail.com>
2022-08-08 18:56:37 -0600

    | |

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -1,5 +1,6 @@
 ﻿"use strict";
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+connection.start();
 //Disable the send button until connection is established.
 //document.getElementById("sendButton").disabled = true;
 //connection.on("ReceiveMessage", function (user, message) {
